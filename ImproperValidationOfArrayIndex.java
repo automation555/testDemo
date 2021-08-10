c1b03d6cbc54f647783ae99b1c50136bb49ccda3
Pooja0509 <80098293+Pooja0509@users.noreply.github.com>
2021-08-10 12:35:51 +0530

    Update ImproperValidationOfArrayIndex.java

--- ImproperValidationOfArrayIndex.java
+++ ImproperValidationOfArrayIndex.java
@@ -2,7 +2,6 @@
 public class ImproperValidationOfArrayIndex{
     static int badCase(int data1) throws Throwable{
         int array[] = { 0, 1, 2, 3, 4 };
-        // EMB-ISSUE: CodeIssueNames.IMPROPER_VALIDATION_OF_ARRAYINDEX
         if (data1 < array.length)
         {
             System.out.println(array[data1]);
@@ -11,19 +10,15 @@ public class ImproperValidationOfArrayIndex{
     }
 
     static int badCase1(int data1) throws Throwable{
-        int array[] = { 0, 1, 2, 3, 4 };
-        // EMB-ISSUE: CodeIssueNames.IMPROPER_VALIDATION_OF_ARRAYINDEX/no-detect
-        if (data1 >= 0) {
+        int array[] = { 0, 1, 2, 3, 4 };  
             if (data1 < array.length) {
                 System.out.println(array[data1]);
             }
             return data1;
         }
-    }
 
     static int goodCase(int data) throws Throwable{
         int array[] = { 0, 1, 2, 3, 4 };
-        // EMB-ISSUE: CodeIssueNames.IMPROPER_VALIDATION_OF_ARRAYINDEX/no-detect
         if (data >= 0 && data < array.length)
         {
             System.out.println(array[data]);
